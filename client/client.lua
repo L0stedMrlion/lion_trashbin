bb152d1c431f5c8a867877dcb008c5d9a2ccfe55
Mrlion <mrlion4202@gmail.com>
2024-06-18 14:54:48 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,12 @@
 local props = Config.Props
-local target = Config.Target
 
 RegisterNetEvent('lion_trashbin:openTempStash', function(stashId)
     exports.ox_inventory:openInventory('stash', stashId)
 end)
 
-exports.ox_target:addModel(props, target)
+if Config.Target == "ox_target" then 
+    exports.ox_target:addModel(props, Config.OXTarget) 
+end
+if Config.Target == "qb-target" then
+    exports['qb-target']:AddTargetModel(props, Config.QBTarget)
+end
