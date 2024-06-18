7ce159a7ba6307af2e7e06a7acd979309f17f85e
Mrlion <mrlion4202@gmail.com>
2024-06-18 14:54:35 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,25 @@
 Config = {}
-
 -- Trash bin inventory settings
 Config.InventoryLabel = "Trash"
 Config.InventorySlots = 5
 Config.InventoryWeight = 5000 -- In grams
-Config.Target = {
+Config.Target = "ox_target" -- ox_target or qb-target
+Config.OXTarget = { -- ONLY USE IF YOU USE OX_TARGET!
     label = "Open Trash Bin",
     icon = "fa-solid fa-trash-can",
     distance = 2.0,
-    --group = {['police']= 0, ['sheriff']= 0 },
     onSelect = function()
         TriggerServerEvent('lion_trashbin:createTempStash')
     end
+}
+Config.QBTarget = { -- ONLY USE IF YOU USE QB-TARGET!
+    {
+        label = "Open Trash Bin",
+        icon = "fa-solid fa-trash-can",
+        action = function(entity)
+            TriggerServerEvent('lion_trashbin:createTempStash')
+        end
+    }
 }
 Config.Props = { -- If you want add or remove props / https://gta-objects.xyz/objects/search?text=bin
     "prop_bin_01a",
